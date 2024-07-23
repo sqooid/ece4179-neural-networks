8d6ea1071265e28da488b0f05a2a318b51b8a390
sqooid <sqooiddev@gmail.com>
2024-07-23 11:14:26 +1000

    first commit

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+  };
+  outputs = { self, nixpkgs, ... }:
+    let
+      forSystem = nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+      pkgsFor = forSystem (system:
+        import nixpkgs { inherit system; }
+      );
+    in
+    {
+      packages = forSystem
+        (system:
+          let
+            pkgs = pkgsFor."${system}";
+          in
+          {
+            # app = pkgs.callPackage ./path { };
+          }
+        );
+      devShells = forSystem
+        (system:
+          let
+            pkgs = pkgsFor."${system}";
+            pp = ps: with ps; [
+              jupyterlab
+              opencv4
+              torch
+              torchaudio
+              torchvision
+              numpy
+              matplotlib
+              scipy
+              numba
+              ipywidgets
+              librosa
+              scikit-image
+              scikit-learn
+              pillow
+              transformers
+              imageio
+              pandas
+            ];
+          in
+          {
+            default = pkgs.mkShell {
+              buildInputs = with pkgs; [ (python312.withPackages pp) ];
+            };
+          }
+        );
+    };
+}
